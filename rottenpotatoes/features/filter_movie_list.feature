6d091f9b7fcf46c19ce1f6cfdb961b42caf632e0
Thomas Nix <tnix2@uncc.edu>
2016-11-09 20:51:41 +0000

    Current problem with capybara functions being undefined on use (see movie_steps.rb)

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -21,12 +21,36 @@ Background: movies have been added to database
 
   And  I am on the RottenPotatoes home page
 
-Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
+Scenario: restrict to movies with "PG" or "R" ratings
+  # enter step(s) to check the "PG" and "R" checkboxes
+  When I check the following ratings: "PG" and "R"
   # enter step(s) to uncheck all other checkboxes
+  When I uncheck the following ratings: "G" and "PG-13"
   # enter step to "submit" the search form on the homepage
+  When I press "Submit"
   # enter step(s) to ensure that PG and R movies are visible
+ Then I should see "The Terminator"
+  Then I should see "When Harry Met Sally"
+  Then I should see "Amelie"
+  Then I should see "The Incredibles"
+  Then I should see "Raiders of the Lost Ark"
   # enter step(s) to ensure that other movies are not visible
+  Then I should not see "Aladdin"
+  Then I should not see "The Help"
+  Then I should not see "Chocolat"
+  Then I should not see "2001: A Space Odyssey"
+  Then I should not see "Chicken Run"
 
 Scenario: all ratings selected
   # see assignment
+  When I check the following ratings: "G,PG,PG-13,R"
+  Then I should see "The Terminator"
+  Then I should see "When Harry Met Sally"
+  Then I should see "Amelie"
+  Then I should see "The Incredibles"
+  Then I should see "Raiders of the Lost Ark"
+  Then I should see "Aladdin"
+  Then I should see "The Help"
+  Then I should see "Chocolat"
+  Then I should see "2001: A Space Odyssey"
+  Then I should see "Chicken Run"
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,11 +1,13 @@
-# Add a declarative step here for populating the DB with movies.
 
-Given /the following movies exist/ do |movies_table|
+Given /the following movies exist:/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    visit('/')
+    click_link('Add new movie')
+  
+    fail "Unimplemented"
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +16,9 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  if page.body.index(e1) >= page.body.index(e2)
+    fail "Unimplemented"
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +29,27 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  split_list = rating_list.split(',')
+  split_list.each do |item|
+    When /^(?:|I )check "([^"]*)"$/ do |field|
+      check(uncheck)
+    end
+
+    When /^(?:|I )uncheck "([^"]*)"$/ do |field|
+      uncheck(uncheck)
+    end
+  end
+  
   fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+  @movies.each do |movie|
+    table_includes = page.body.table.indluce? (movie) ? true : false
+  end
+  
+  if (!table_includes)
+    fail "Unimplemented"
+  end
 end
