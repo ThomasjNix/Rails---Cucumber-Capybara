17fef006c8cb0094be565be7d3f7e3497f8c7e3a
Thomas Nix <tnix2@uncc.edu>
2016-11-10 13:31:37 +0000

    Problems resolved using bundle command; cucumber and capybara functioning 100% now. Assignment complete.

--- rottenpotatoes/features/filter_movie_list.feature
+++ rottenpotatoes/features/filter_movie_list.feature
@@ -23,11 +23,11 @@ Background: movies have been added to database
 
 Scenario: restrict to movies with "PG" or "R" ratings
   # enter step(s) to check the "PG" and "R" checkboxes
-  When I check the following ratings: "PG" and "R"
+  When I check the following ratings: "PG,R"
   # enter step(s) to uncheck all other checkboxes
-  When I uncheck the following ratings: "G" and "PG-13"
+  When I uncheck the following ratings: "G,PG-13"
   # enter step to "submit" the search form on the homepage
-  When I press "Submit"
+  When I press "Refresh"
   # enter step(s) to ensure that PG and R movies are visible
  Then I should see "The Terminator"
   Then I should see "When Harry Met Sally"
--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,55 +1,30 @@
 
 Given /the following movies exist:/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
-    visit('/')
-    click_link('Add new movie')
-  
-    fail "Unimplemented"
+    Movie.create!(title: movie["title"], rating: movie["rating"], release_date: movie["release_date"])
   end
 end
-
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  if page.body.index(e1) >= page.body.index(e2)
-    fail "Unimplemented"
-  end
+   expect(page.body) =~ (/#{e1}.+#{e2}/)
 end
 
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
   split_list = rating_list.split(',')
-  split_list.each do |item|
-    When /^(?:|I )check "([^"]*)"$/ do |field|
-      check(uncheck)
+  split_list.each do |element|
+    element.gsub!('"','')
+  end
+  if uncheck  
+    split_list.each do |item|
+     uncheck "ratings_#{item}"
     end
-
-    When /^(?:|I )uncheck "([^"]*)"$/ do |field|
-      uncheck(uncheck)
+  else
+    split_list.each do |item|
+     check "ratings_#{item}"
     end
   end
-  
-  fail "Unimplemented"
 end
 
 Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  @movies.each do |movie|
-    table_includes = page.body.table.indluce? (movie) ? true : false
-  end
-  
-  if (!table_includes)
-    fail "Unimplemented"
-  end
+  expect(page).to have_content(movie["title"])
 end
